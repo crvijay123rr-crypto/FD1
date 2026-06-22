4246fc3b70d8afd6f7c5580f5a1ec668c8cc1c29
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-06-22 06:40:37 +0530

    Create config.py with API and MongoDB settings
    Added configuration settings for API and database.

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+API_ID = 1234567       # Apna API ID dalein
+API_HASH = "your_api_hash_here"
+MASTER_BOT_TOKEN = "your_master_bot_token_here" 
+
+# MongoDB URL (Cluster ya localhost ka link yahan dalein)
+MONGO_URL = "mongodb+srv://your_username:your_password@cluster.mongodb.net/?retryWrites=true&w=majority"
+DB_NAME = "premium_forward_db"
